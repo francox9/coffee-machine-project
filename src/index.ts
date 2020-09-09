f950dfb5d23ad643266129a159191789a274120f
francox9 <sunxufei92@gmail.com>
2020-09-09 21:50:44 +1000

    iter2

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 export class DrinkMaker {
     static message: string
-    // constructor() {
 
-    // }
+    orderValue(instruction: string): number {
+        const [ type, ...amounts ] = instruction.split(':')
+        switch (type) {
+            case 'M': return 0
+            case 'T': return 0.4
+            case 'H': return 0.5
+            case 'C': return 0.6
+            default: throw new Error('Undefined drink type')
+        }
+    }
     
-    order(instruction: string) {
-        DrinkMaker.message = instruction
+    order(instruction: string, payment: number) {
+        const value = this.orderValue(instruction)
+
+        if (value > payment) DrinkMaker.message = 'Payment not enough'
+        else DrinkMaker.message = instruction
     }
 }
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -6,9 +6,17 @@ describe('Order', () => {
         drinkMaker = new DrinkMaker()
     })
 
-    it('should store the order instruction', () => {
+    it('should display order detail', () => {
         const instruction = 'T:1:0'
-        drinkMaker.order(instruction)
+        const payment = 0.4
+        drinkMaker.order(instruction, payment)
         expect(DrinkMaker.message).toEqual(instruction)
+    })
+
+    it('should throw display error message if payment is lower than selling price', () => {
+        const instruction = 'T:1:0'
+        const payment = 0.3
+        drinkMaker.order(instruction, payment)
+        expect(DrinkMaker.message).toEqual('Payment not enough')
     })
 })
