50037ebb77a516f0995856ac1a99a0a8bf57d26d
francox9 <sunxufei92@gmail.com>
2020-09-10 21:17:31 +1000

    itr 4 done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
 type OrderType = 'coffee' | 'tea' | 'chocolate' | 'message' | 'orange'
-class Order {
+interface DailyReport {
+    money: number
+    coffeeAmount: number
+    teaAmount: number
+    chocolateAmount: number
+    orangeAmount: number
+}
+const defaultReport: DailyReport = {
+    money: 0,
+    coffeeAmount: 0,
+    teaAmount: 0,
+    chocolateAmount: 0,
+    orangeAmount: 0
+}
+export class Order {
+    static Report: DailyReport = { ...defaultReport }
+    static resetReport() {
+        this.Report = { ...defaultReport }
+    }
+
     type: OrderType
     sugar: number
     stick: number
     value: number
     extraHot = false
-    constructor(description: string) {
+    constructor(description: string, payment: number) {
         const [drinkDesc, ...amounts] = description.split(':')
         if (/^\wh$/.test(drinkDesc)) this.extraHot = true
         const type = drinkDesc[0]
@@ -19,47 +38,61 @@ class Order {
                 break;
             }
             case 'T': {
+                if (payment < 0.4) throw new Error('payment-insufficient')
                 this.value = 0.4
                 this.type = 'tea'
                 this.sugar = parseInt(amounts[0]) || 0
                 this.stick = this.sugar > 0? 1: 0
+                Order.Report.teaAmount++
                 break;
             }
             case 'H': {
+                if (payment < 0.5) throw new Error('payment-insufficient')
                 this.value = 0.5
                 this.type = 'chocolate'
                 this.sugar = parseInt(amounts[0]) || 0
                 this.stick = this.sugar > 0? 1: 0
+                Order.Report.chocolateAmount++
                 break;
             }
             case 'C': {
+                if (payment < 0.6) throw new Error('payment-insufficient')
                 this.value = 0.6
                 this.type = 'coffee'
                 this.sugar = parseInt(amounts[0]) || 0
                 this.stick = this.sugar > 0? 1: 0
+                Order.Report.coffeeAmount++
                 break;
             }
             case 'O': {
+                if (payment < 0.6) throw new Error('payment-insufficient')
                 this.value = 0.6
                 this.type = 'orange'
                 this.sugar = parseInt(amounts[0]) || 0
                 this.stick = this.sugar > 0 ? 1 : 0
+                Order.Report.orangeAmount++
                 break;
             }
-            default: throw new Error('Undefined drink type')
+            default: throw new Error('undefined-drink')
         }
+
+        Order.Report.money += this.value
     }
 }
 
 export class DrinkMaker {
     static message: string
 
-    order(instruction: string, payment: number) {
-        const order = new Order(instruction)
-
-        if (order.value > payment) DrinkMaker.message = 'Payment not enough'
-        else DrinkMaker.message = instruction
-
+    order(instruction: string, payment: number): Order {
+        let order = null
+        try {
+            order = new Order(instruction, payment)
+            DrinkMaker.message = instruction
+        }
+        catch(e) {
+            if (e.message === 'payment-insufficient') DrinkMaker.message = 'Payment not enough'
+            else if (e.message === 'undefined-drink') DrinkMaker.message = 'Undefined drink type'
+        }
         return order
     }
 }
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,9 +1,10 @@
-import { DrinkMaker } from '../src/index'
+import { DrinkMaker, Order } from '../src/index'
 
 describe('Order', () => {
     let drinkMaker
     beforeEach(() => {
         drinkMaker = new DrinkMaker()
+        Order.resetReport()
     })
 
     it('should display order detail', () => {
@@ -43,6 +44,7 @@ describe('Order', () => {
             const order = drinkMaker.order(instruction, payment)
             expect(DrinkMaker.message).toEqual(instruction)
             expect(order.extraHot).toEqual(true)
+            expect(order.type).toEqual('coffee')
         })
         it('should work for hot chocolate', () => {
             const instruction = 'Hh:1:0'
@@ -50,6 +52,7 @@ describe('Order', () => {
             const order = drinkMaker.order(instruction, payment)
             expect(DrinkMaker.message).toEqual(instruction)
             expect(order.extraHot).toEqual(true)
+            expect(order.type).toEqual('chocolate')
         })
         it('should work for tea', () => {
             const instruction = 'Th:2:0'
@@ -57,6 +60,77 @@ describe('Order', () => {
             const order = drinkMaker.order(instruction, payment)
             expect(DrinkMaker.message).toEqual(instruction)
             expect(order.extraHot).toEqual(true)
+            expect(order.type).toEqual('tea')
+        })
+    })
+
+    describe('Report', () => {
+        it('should work if we sold nothing', () => {
+            expect(Order.Report.money).toEqual(0)
+            expect(Order.Report.coffeeAmount).toEqual(0)
+            expect(Order.Report.teaAmount).toEqual(0)
+            expect(Order.Report.chocolateAmount).toEqual(0)
+            expect(Order.Report.orangeAmount).toEqual(0)
+        })
+
+        it('should not be influenced by messages', () => {
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('M:random messages 1', 0)
+            drinkMaker.order('M:random messages 2', 0)
+
+            expect(Order.Report.money).toEqual(0.6 * 1)
+            expect(Order.Report.coffeeAmount).toEqual(1)
+            expect(Order.Report.teaAmount).toEqual(0)
+            expect(Order.Report.chocolateAmount).toEqual(0)
+            expect(Order.Report.orangeAmount).toEqual(0)
+        })
+
+        it('should yield proper amount of money if orders are made', () => {
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('H::', 0.5)
+            drinkMaker.order('H::', 0.5)
+            drinkMaker.order('O::', 0.6)
+            drinkMaker.order('T::', 0.4)
+
+            expect(Order.Report.money).toEqual(0.6 * 3 + 0.5 * 2 + 0.6 * 1 + 0.4 * 1)
+            expect(Order.Report.coffeeAmount).toEqual(3)
+            expect(Order.Report.teaAmount).toEqual(1)
+            expect(Order.Report.chocolateAmount).toEqual(2)
+            expect(Order.Report.orangeAmount).toEqual(1)
+        })
+
+        it('should calculate by prices, rather than payment amounts', () => {
+            drinkMaker.order('C::', 1)
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('C::', 2)
+            drinkMaker.order('H::', 0.5)
+            drinkMaker.order('H::', 0.6)
+            drinkMaker.order('O::', 0.8)
+            drinkMaker.order('T::', 0.4)
+
+            expect(Order.Report.money).toEqual(0.6 * 3 + 0.5 * 2 + 0.6 * 1 + 0.4 * 1)
+            expect(Order.Report.coffeeAmount).toEqual(3)
+            expect(Order.Report.teaAmount).toEqual(1)
+            expect(Order.Report.chocolateAmount).toEqual(2)
+            expect(Order.Report.orangeAmount).toEqual(1)
+        })
+
+        it('should not include orders rejected by insufficient payment', () => {
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('C::', 0.3)
+            drinkMaker.order('C::', 0.6)
+            drinkMaker.order('H::', 0.5)
+            drinkMaker.order('H::', 0.1)
+            drinkMaker.order('O::', 0.6)
+            drinkMaker.order('T::', 0.35)
+
+            expect(Order.Report.money).toEqual(0.6 * 2 + 0.5 * 1 + 0.6 * 1)
+            expect(Order.Report.coffeeAmount).toEqual(2)
+            expect(Order.Report.teaAmount).toEqual(0)
+            expect(Order.Report.chocolateAmount).toEqual(1)
+            expect(Order.Report.orangeAmount).toEqual(1)
         })
     })
 
