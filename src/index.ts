189d1bef5589e018e15f82995d8b6ed84cf3d099
francox9 <sunxufei92@gmail.com>
2020-09-10 20:39:08 +1000

    itr 3 done

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,66 @@
-export class DrinkMaker {
-    static message: string
-
-    orderValue(instruction: string): number {
-        const [ type, ...amounts ] = instruction.split(':')
+type OrderType = 'coffee' | 'tea' | 'chocolate' | 'message' | 'orange'
+class Order {
+    type: OrderType
+    sugar: number
+    stick: number
+    value: number
+    extraHot = false
+    constructor(description: string) {
+        const [drinkDesc, ...amounts] = description.split(':')
+        if (/^\wh$/.test(drinkDesc)) this.extraHot = true
+        const type = drinkDesc[0]
+        
         switch (type) {
-            case 'M': return 0
-            case 'T': return 0.4
-            case 'H': return 0.5
-            case 'C': return 0.6
+            case 'M': {
+                this.value = 0
+                this.type = 'message'
+                this.stick = 0
+                this.sugar = 0
+                break;
+            }
+            case 'T': {
+                this.value = 0.4
+                this.type = 'tea'
+                this.sugar = parseInt(amounts[0]) || 0
+                this.stick = this.sugar > 0? 1: 0
+                break;
+            }
+            case 'H': {
+                this.value = 0.5
+                this.type = 'chocolate'
+                this.sugar = parseInt(amounts[0]) || 0
+                this.stick = this.sugar > 0? 1: 0
+                break;
+            }
+            case 'C': {
+                this.value = 0.6
+                this.type = 'coffee'
+                this.sugar = parseInt(amounts[0]) || 0
+                this.stick = this.sugar > 0? 1: 0
+                break;
+            }
+            case 'O': {
+                this.value = 0.6
+                this.type = 'orange'
+                this.sugar = parseInt(amounts[0]) || 0
+                this.stick = this.sugar > 0 ? 1 : 0
+                break;
+            }
             default: throw new Error('Undefined drink type')
         }
     }
-    
-    order(instruction: string, payment: number) {
-        const value = this.orderValue(instruction)
+}
 
-        if (value > payment) DrinkMaker.message = 'Payment not enough'
+export class DrinkMaker {
+    static message: string
+
+    order(instruction: string, payment: number) {
+        const order = new Order(instruction)
+
+        if (order.value > payment) DrinkMaker.message = 'Payment not enough'
         else DrinkMaker.message = instruction
+
+        return order
     }
 }
 
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -19,4 +19,45 @@ describe('Order', () => {
         drinkMaker.order(instruction, payment)
         expect(DrinkMaker.message).toEqual('Payment not enough')
     })
+
+    describe('Orange juice order', () => {
+        it('should be handled properly when payment is correct', () => {
+            const instruction = 'O::'
+            const payment = 0.6
+            drinkMaker.order(instruction, payment)
+            expect(DrinkMaker.message).toEqual(instruction)
+        })
+
+        it('should throw error when payment is insufficient', () => {
+            const instruction = 'O::'
+            const payment = 0.5
+            drinkMaker.order(instruction, payment)
+            expect(DrinkMaker.message).toEqual('Payment not enough')
+        })
+    })
+
+    describe('Extra hot order', () => {
+        it('should work for coffee', () => {
+            const instruction = 'Ch::'
+            const payment = 0.6
+            const order = drinkMaker.order(instruction, payment)
+            expect(DrinkMaker.message).toEqual(instruction)
+            expect(order.extraHot).toEqual(true)
+        })
+        it('should work for hot chocolate', () => {
+            const instruction = 'Hh:1:0'
+            const payment = 0.5
+            const order = drinkMaker.order(instruction, payment)
+            expect(DrinkMaker.message).toEqual(instruction)
+            expect(order.extraHot).toEqual(true)
+        })
+        it('should work for tea', () => {
+            const instruction = 'Th:2:0'
+            const payment = 0.4
+            const order = drinkMaker.order(instruction, payment)
+            expect(DrinkMaker.message).toEqual(instruction)
+            expect(order.extraHot).toEqual(true)
+        })
+    })
+
 })
